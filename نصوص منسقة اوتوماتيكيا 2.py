fc19ca3ac231bca5e73f96cc33ccac6799080796
rimabohamdan <151973213+rimabohamdan@users.noreply.github.com>
2024-03-10 09:35:26 -0600

    Add files via upload
    Automatically formatted texts 2

---
+++ نصوص منسقة اوتوماتيكيا 2.py
@@ -0,0 +1,8 @@
+age = input (" How old are you ? \n")
+int_old = int ( age )
+year = 2024
+bron_in = year - int_old
+str_born_in = str (bron_in )
+print (' You were born in the year : ' + str_born_in )
+#طريقة ثانية نحذف السطر الاخير ونستبدل بالسطر التالي
+#print (f" Your age is :   + {age}")
