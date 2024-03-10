d2ec5ed08065dd2668bcbaa931ef48b91ec5e88e
rimabohamdan <151973213+rimabohamdan@users.noreply.github.com>
2024-03-10 09:35:02 -0600

    Add files via upload
    Automatically formatted texts 1

---
+++ نصوص منسقة اوتوماتيكيا 1.py
@@ -0,0 +1,6 @@
+old = input ("How old are you ? \n")
+int_old = int (old)
+years_now = int (2024)
+#السنة الي ولد فيها المستخدم
+born_your = int (years_now - int_old)
+print("You weere bron in the year  " + str (born_your))
