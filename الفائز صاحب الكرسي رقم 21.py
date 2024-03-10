d2bbb158ea5707a86f9daf571c7a2a1df28ce809
rimabohamdan <151973213+rimabohamdan@users.noreply.github.com>
2024-03-10 09:36:22 -0600

    Add files via upload
    Lottery winner

---
+++ الفائز صاحب الكرسي رقم 21.py
@@ -0,0 +1,11 @@
+chair_number = int (input ( "Enter your chair number :\n" ))
+if chair_number == 21 :
+    print("You win")
+else :
+    print(" Sorry ")
+#لو حبينا نربح  الكل ونستبعد رقم معين 13 ع سبيل المثال 
+#chair_number = int(input?(" Enter your chair number : \n " ))
+    #if chair_number =! 13 :
+    #   print ("You win")
+    #else :
+    #   print (" Sorry ")
