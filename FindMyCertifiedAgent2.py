bd68272fe4698ce66e0a550df00b57149af9a771
rimabohamdan <151973213+rimabohamdan@users.noreply.github.com>
2024-03-10 09:57:41 -0600

    Create FindMyCertifiedAgent2.py
    Create FindMyCertifiedAgent2.py

---
+++ FindMyCertifiedAgent2.py
@@ -0,0 +1,5 @@
+area = input (" Chose an area : (Aleppo),(Damascus),or (sweida) \n")
+if area.lower() == "aleppo" or area.lower() == "sweida" or area.lower() == "Damascus" :
+  print (f"{area} is on our list ! ")
+else :
+  print (f"{area} is not on our list !")
